0ed9f35692543d98797f03b912a8e5f4deb1c73c
NguyenTanTai <ntai111206@gm,ail.com>
2026-02-12 01:57:47 +0000

    refactor: Update ThemeProvider import path to use kebab-case filename.

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
-import { ThemeProvider } from './contexts/ThemeContext';
+import { ThemeProvider } from './contexts/theme-context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
